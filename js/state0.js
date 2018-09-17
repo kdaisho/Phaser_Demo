072e13462ebc39656f4a1fdaf1bec940ab65134b
Daisho <daishokomiyama@gmail.com>
2018-09-16 20:46:16 -0400

    Adding bg

--- js/state0.js
+++ js/state0.js
@@ -5,29 +5,43 @@ const keys = [
 const centerX = 1500 / 2;
 const centerY = 1000 / 2;
 let daisho;
-let speed = 4;
+let speed = 6;
 
 demo.state0 = function() {};
 demo.state0.prototype = {
     preload: function() {
         game.load.image('daisho', 'game-assets/sprites/daisho.png');
+        game.load.image('trees', 'game-assets/backgrounds/trees.png');
     },
     create: function() {
+        game.physics.startSystem(Phaser.Physics.ARCADE);
         game.stage.backgroundColor = '#d32f2f';
         addChangeStateEventListener(keys);
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-
+        game.world.setBounds(0, 0, 2813, 1000);
+        const treeBG = game.add.sprite(0, 0, 'trees');
         daisho = game.add.sprite(centerX, centerY, 'daisho');
         daisho.anchor.setTo(.5, .5);
+        daisho.scale.setTo(.7, .7);
+        game.physics.enable(daisho);
+        daisho.body.collideWorldBounds = true;
+
+        game.camera.follow(daisho);
+        game.camera.deadzone = new Phaser.Rectangle(centerX - 300, 0, 600, 1000);
     },
     update: function() {
         if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+            daisho.scale.setTo(.7, .7);
             daisho.x += speed;
         }
         else if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+            daisho.scale.setTo(-.7, .7);
             daisho.x -= speed;
         }
         if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+            if (daisho.y < 318) {
+                daisho.y = 318;
+            }
             daisho.y -= speed;
         }
         else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
